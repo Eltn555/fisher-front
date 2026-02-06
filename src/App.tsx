f1527efd3e3c2096487ff988cc0cda9cff4aa6cf
Eldor <eldorsobitov18@gmail.com>
2026-02-06 16:42:03 +0500

    Add ToastContainer from react-toastify for user notifications in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Form from './components/main'
 import UserManagement from './components/UserManagement'
 
@@ -9,6 +11,18 @@ function App() {
         <Route path="/" element={<Form />} />
         <Route path="/userManagement" element={<UserManagement />} />
       </Routes>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </HashRouter>
   )
 }
